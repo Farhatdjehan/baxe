14c15f558dd3d42f23530a3dfd35ed07bfd02366
farhatdk <farhatdk98@gmail.com>
2021-09-01 17:44:15 +0700

    deployment

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import Head from "next/head"
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'react-notifications/lib/notifications.css'
-import '../styles/globals.css'
+import Head from "next/head";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-notifications/lib/notifications.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -9,42 +9,127 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>{`BAXE - The world’s most innovative digital gateway platform.`}</title>
         <link rel="icon" href="/favicon.ico/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="57x57" href="/favicon.ico/apple-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="/favicon.ico/apple-icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/favicon.ico/apple-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/favicon.ico/apple-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/favicon.ico/apple-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/favicon.ico/apple-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/favicon.ico/apple-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/favicon.ico/apple-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico/apple-icon-180x180.png" />
-        <link rel="icon" type="image/png" sizes="192x192"  href="/favicon.ico/android-icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon.ico/favicon-96x96.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/favicon.ico/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/favicon.ico/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/favicon.ico/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/favicon.ico/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/favicon.ico/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/favicon.ico/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/favicon.ico/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/favicon.ico/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon.ico/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon.ico/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon.ico/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon.ico/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon.ico/favicon-16x16.png"
+        />
         <link rel="manifest" href="/favicon.ico/manifest.json" />
         <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/favicon.ico/ms-icon-144x144.png" />
+        <meta
+          name="msapplication-TileImage"
+          content="/favicon.ico/ms-icon-144x144.png"
+        />
         <meta name="theme-color" content="#ffffff"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <meta property="og:image" content="https://res.cloudinary.com/telecreativenow/image/upload/c_scale,q_60,w_951/v1625505271/BAXE/baxe-banner.png" />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/telecreativenow/image/upload/c_scale,q_60,w_951/v1625505271/BAXE/baxe-banner.png"
+        />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://baxe.com" />
-        <meta property="og:title" content="BAXE - The world’s most innovative digital gateway platform." />
-        <meta property="og:description" content="BAXE is more than just a digital currency. It’s the gateway between traditional financial systems and the emerging digital economy." />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
-        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <meta
+          name="facebook-domain-verification"
+          content="qmqndao91m3dfgjl2x5cx4sbjvji0l"
+        />
+        <meta
+          property="og:title"
+          content="BAXE - The world’s most innovative digital gateway platform."
+        />
+        <meta
+          property="og:description"
+          content="BAXE is more than just a digital currency. It’s the gateway between traditional financial systems and the emerging digital economy."
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WQVC9Z8');`}}></script>
-
+        })(window,document,'script','dataLayer','GTM-WQVC9Z8');`,
+          }}
+        ></script>
       </Head>
       <Component {...pageProps} />
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQVC9Z8" height="0" width="0" style={{ display:'none', visibility: 'hidden' }}></iframe></noscript>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-WQVC9Z8"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,60 @@
-import React, { useState } from 'react'
-import { Container } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
 
-import { Header, Footer, Footer2, VideoBAXE } from '../components/partials'
+import DocumentMeta from "react-document-meta";
+import { Header, Footer, Footer2, VideoBAXE } from "../components/partials";
 
-import Banner from '../components/sections/Banner'
-import Welcome from '../components/sections/Welcome'
-import DownloadApp from '../components/sections/DownloadApp'
-import Features from '../components/sections/Features'
+import Banner from "../components/sections/Banner";
+import Welcome from "../components/sections/Welcome";
+import DownloadApp from "../components/sections/DownloadApp";
+import Features from "../components/sections/Features";
 
 export default function Home() {
-  const [showVideo, setShowVideo] = useState(false)
+  const [showVideo, setShowVideo] = useState(false);
 
   const closeVideo1 = () => {
-    setShowVideo(false)
-  }
+    setShowVideo(false);
+  };
 
   const openVideo1 = () => {
-    setShowVideo(true)
-  }
+    setShowVideo(true);
+  };
 
   const openVideo2 = () => {
-    setShowVideo(true)
-  }
-  
+    setShowVideo(true);
+  };
+  const meta = {
+    meta: {
+      name: "facebook-domain-verification",
+      content: "qmqndao91m3dfgjl2x5cx4sbjvji0l",
+      charset: "utf-8",
+    },
+  };
   return (
     <>
-      <VideoBAXE
-        isVideoShow={showVideo}
-        onClickBackground={closeVideo1}
-        onEndVideo={closeVideo1}
-        videoID="L-zgF2f86i8"
-      />
-      <VideoBAXE
-        isVideoShow={showVideo}
-        onClickBackground={closeVideo1}
-        onEndVideo={closeVideo1}
-        videoID="3_2065uJzcU"
-      />
-      <Header />
-      <Container fluid>
-        <Banner onClickLearnMore={openVideo1} />
-      </Container>
-      <Welcome onClickWatchNow={openVideo2} />
-      <DownloadApp />
-      <Features onClickWatchNow={openVideo2} />
-      <Footer />
-      <Footer2 />
+      <DocumentMeta {...meta}>
+        <VideoBAXE
+          isVideoShow={showVideo}
+          onClickBackground={closeVideo1}
+          onEndVideo={closeVideo1}
+          videoID="L-zgF2f86i8"
+        />
+        <VideoBAXE
+          isVideoShow={showVideo}
+          onClickBackground={closeVideo1}
+          onEndVideo={closeVideo1}
+          videoID="3_2065uJzcU"
+        />
+        <Header />
+        <Container fluid>
+          <Banner onClickLearnMore={openVideo1} />
+        </Container>
+        <Welcome onClickWatchNow={openVideo2} />
+        <DownloadApp />
+        <Features onClickWatchNow={openVideo2} />
+        <Footer />
+        <Footer2 />
+      </DocumentMeta>
     </>
-  )  
+  );
 }
